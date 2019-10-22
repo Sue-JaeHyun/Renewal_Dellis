a6b82c99e44af2df98deba1bbd02ce41079b4c59
Sue-JaeHyun <aroa96@naver.com>
2019-10-23 04:53:24 +0900

    first

--- app/src/main/java/com/example/renewal_dellis/MainActivity.java
+++ app/src/main/java/com/example/renewal_dellis/MainActivity.java
@@ -1,58 +1,68 @@
 package com.example.renewal_dellis;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.TextureView;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button sendbt;
-    private EditText editdt;
-    public String msg;
-    private TextView textView;
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference mReference;
-    private ChildEventListener mChild;
+    private static final String TAG = "MainActivity";
+    private FirebaseFirestore db;
+    TextView tvMessage;
+    EditText etNewMessage;
+    Button btUpdate;
 
-    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference = firebaseDatabase.getReference();
-    ListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("");
+        tvMessage = (TextView) findViewById(R.id.tv_message);
+        etNewMessage = (EditText) findViewById(R.id.et_newData);
+        btUpdate = (Button) findViewById(R.id.bt_update);
+        db = FirebaseFirestore.getInstance();
 
-
-        sendbt = (Button) findViewById(R.id.button2);
-        editdt = (EditText) findViewById(R.id.editText);
-        sendbt.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                // 버튼 누르면 수행 할 명령
-                msg = editdt.getText().toString();
-                databaseReference.child("Sig_Item").push().setValue(msg);
+        //버튼 이벤트
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String newMessage = etNewMessage.getText().toString().trim();
+                db.collection("sig_item").document(newMessage).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if( documentSnapshot.exists())
+                        {
+                            Map<String,Object>result = documentSnapshot.getData();
+//                                Log.d("ABCDE",documentSnapshot.getData().get("name").toString());
+                            tvMessage.setText(((Map<String,Object>)result.get("components")).get("anthena_short").toString());
+                        }
+                    }
+                });
             }
-        });
-    }
-    }
 
+
+        });
+
+
+    }
+}
+/*
+1. firebase에서 데이터 형식 먼저 만든다.(문서-일련번호 추가, 필드-부수기재추가)
+2. 레이아웃 만들기(과거 아웃사이더 매니지먼트 참고)
+3. 레이아웃에 나눈 형식대로 값이 출력되어야함.'
+4. 이해완료
+ */
